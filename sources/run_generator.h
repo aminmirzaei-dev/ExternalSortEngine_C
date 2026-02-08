018ca076362a331d6fbcf3bdc22ebe78bd8ac683
Amin Mirzaei <aminmirzaeidev@gmail.com>
2026-02-08 03:34:51 +0330

    Add generate_runs function to run_generator.h
    Added function declaration for generate_runs.

--- sources/run_generator.h
+++ sources/run_generator.h
@@ -1,1 +1,16 @@
+#ifndef RUN_GENERATOR_H
+#define RUN_GENERATOR_H
+
+#include <stddef.h>
+
+/*
+ * Generates sorted runs from the input file.
+ *
+ * Returns the number of runs created, or -1 on failure.
+ */
+int generate_runs(const char *input_file,
+                  size_t memory_limit,
+                  char ***run_files);
+
+#endif 
 
