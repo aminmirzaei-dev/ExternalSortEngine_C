2c172079c1048d193da1f84e9e9fe70b4daf064b
Amin Mirzaei <aminmirzaeidev@gmail.com>
2026-02-08 03:37:01 +0330

    Add external sort test in test_external_sort.c

--- tests/test_external_sort.c
+++ tests/test_external_sort.c
@@ -1,1 +1,10 @@
+#include "../include/externalsortengine.h"
+#include <stdio.h>
+
+int main(void)
+{
+    external_sort("input.txt", "output.txt", 5);
+    printf("External sort completed\n");
+    return 0;
+}
 
